f13cb20fe650a712b1c6ee229f022508caebd0dc
Malaya Tiwari <sidzilegend@gmail.com>
2024-12-05 20:29:06 +0530

    update: node modules

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello this is express server");
 });
 
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
