694d2f359b0bcc0b9a3721226efd6f7cb3ca4d75
Malaya Tiwari <sidzilegend@gmail.com>
2024-12-05 19:40:05 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello this is express server");
+});
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
